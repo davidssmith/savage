aa354fd73267939773ab33f24dec36c1333cdb34
dss <dss@xan.lan>
2020-01-21 22:55:42 -0600

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,279 +1,259 @@
+use std::fs::{File};
+use std::io::{self, Read};
 
+#[derive(PartialEq,Debug)]
+enum Side {
+    White,
+    Black,
+}
 
-struct Style {
-    units: u32, // size of square
+#[derive(Debug)]
+struct Board {
+    fen: &'static str,
+
+    units: i32, // relative size of a square
     //board: usize, // = 8*units
-    corner: u32,  // corner radius
-    margin: u32,
-    font: String,
-    fontsize: u8,
+    corner_radius: i32,
+    margin: i32,
+    border: i32,
+
+    font: &'static str,
+    font_size: i32,
+    pieces: &'static str,
+    color_light: &'static str,
+    color_dark: &'static str,
+
     show_coords: bool,
     flip: bool,
     show_indicator: bool,
     grayscale: bool,
 }
 
-
-struct Board {
-    style: Style,
-    fen: String,
-    pieces: [char; 64],
-    color_light: [char; 9],
-    color_dark: [char; 9],
+impl Default for Board {
+    fn default() -> Self {
+        let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        let units: i32 = 64;
+        let corner_radius: i32 = 0;
+        let margin: i32 = 48;
+        let border: i32 = 2;
+        let font = "sans-serif";
+        let font_size: i32 = 20;
+        let pieces = "merida";
+        let color_light = "a48c62";
+        let color_dark = "846c40";
+        let show_coords = false;
+        let flip = false;
+        let show_indicator = false;
+        let grayscale = false;
+        Board { fen, units, corner_radius, margin, border, font, font_size, pieces, color_light,
+        color_dark, show_coords, flip, show_indicator, grayscale }
+    }
 }
 
-
-enum Side {
-    White,
-    Black,
+/// Read a file and dump its contents to stdout
+fn cat(filename: &str) -> io::Result<()> {
+    let mut contents = String::new();
+    let f = File::open(filename)?;
+    f.read_to_string(&mut contents);
+    println!("{}", contents);
+    Ok(())
 }
-
 
 impl Board {
+    fn new() -> Board {
+Board::default()
+    }
 
-    fn side_to_move(&self) -> Option<Side> {
-        // find first space in FEN, then first char after that 
+fn side_to_move(&self) -> Option<Side> {
+// find first space in FEN, then first char after that
         // is the side to move. Check if that char is 'w'.
-        let side_char_index = self.fen.find(' ') + 1;
+        let side_char_index = self.fen.find(' ')? + 1;
         match self.fen.chars().nth(side_char_index) {
-            'w' => Some(Side::White),
-            'b' => Some(Side::Black),
+            Some('w') => Some(Side::White),
+            Some('b') => Some(Side::Black),
             _ => None,
         }
     }
 
     fn white_to_move(&self) -> bool {
-        self.side_to_move() == Some(Side::White)
+        self.side_to_move().unwrap() == Side::White
     }
 
     fn black_to_move(&self) -> bool {
-        self.side_to_move() == Some(Side::Black)
+        self.side_to_move().unwrap() == Side::Black
     }
+
+
+fn put_svg_header(&self) {
+    let N = self.units*8 + 2*self.margin;
+	println!("<?xml version='1.0' encoding='utf-8'?>\n\
+           <svg viewBox='0 0 {} {}' style='background-color:#ffffff00' version='1.1' \
+           xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' \
+           xml:space='preserve' x='0px' y='0px' width='{}' height='{}'>", N, N, N, N);
+	println!("<!-- Creator: Savage, Copyright 2020 David S. Smith <david.smith@gmail.com> -->");
+	println!("<!-- FEN: {} -->", self.fen);
 }
 
 
-/// Read a file and dump its contents to stdout
-fn cat(filename: &str) {
-    let mut lines: Vec<u8> = Vec::new();
-    File::new(filename).read(&mut lines);
-    println!("{}", lines);
-}
-
-
-void
-put_svg_header(const char *fen)
-{
-    int N = units*8 + 2*margin;
-	printf("<?xml version='1.0' encoding='utf-8'?>\n"
-           "<svg viewBox='0 0 %d %d' style='background-color:#ffffff00' version='1.1' "
-           "xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' "
-           "xml:space='preserve' x='0px' y='0px' width='%d' height='%d'>\n", N, N, N, N);
-	printf("<!-- Creator: SVGBoard, Copyright 2019 David S. Smith <david.smith@gmail.com> -->\n");
-	printf("<!-- FEN: %s -->\n", fen);
-
-}
-
-
-void
-put_squares ()
-{
-    int N = 8*units;
-
-	
-    //printf("<!-- BOARD MARGIN -->\n");
-	//printf("<rect x='%d' y='%d' width='%d' height='%d' fill='#%s' stroke='#000' stroke-width='%d'/>\n",
-			//0, 0, margin*2+N, margin*2+N, color_dark, border);
-
-    printf("<!-- DARK SQUARES -->\n");
-    if (flags.grayscale) // cross hatched: 
-    {
-		int d = units/12;
-        printf("<pattern id='crosshatch' width='%d' height='%d' "
-               "patternTransform='rotate(45 0 0)' patternUnits='userSpaceOnUse'>\n"
-                 "  <line x1='0' y1='0' x2='0' y2='%d' style='stroke:#000; stroke-width:1' />\n "
-               "</pattern>\n", d, d, d);
-                 //"  <rect x='0' y='0' width='%d' height='%d' fill='#fff' />\n "
-		printf("<pattern id='diagonalHatch' patternUnits='userSpaceOnUse' width='4' height='4'>\n "
-  				"<path d='M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2'\n "
-        		"style='stroke:black; stroke-width:1' />\n "
-				"</pattern>\n");
-        printf("<rect x='%d' y='%d' width='%d' height='%d' fill='url(#crosshatch)' />\n",
-                margin, margin, N, N);
-        strlcpy(color_light, "fff", 4);
+fn put_squares (&self) {
+    let N = 8*self.units;
+    println!("<!-- DARK SQUARES -->");
+    if self.grayscale { // cross hatched:
+		let d = self.units/12;
+        println!("<pattern id='crosshatch' width='{}' height='{}' \
+               patternTransform='rotate(45 0 0)' patternUnits='userSpaceOnUse'>\n\
+                   <line x1='0' y1='0' x2='0' y2='{}' style='stroke:#000; stroke-width:1' />\n \
+               </pattern>", d, d, d);
+                 //"  <rect x='0' y='0' width='{}' height='{}' fill='#fff' />\n "
+		println!("<pattern id='diagonalHatch' patternUnits='userSpaceOnUse' width='4' height='4'>\n \
+  				<path d='M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2'\n \
+        		style='stroke:black; stroke-width:1' />\n \
+				</pattern>");
+        println!("<rect x='{}' y='{}' width='{}' height='{}' fill='url(#crosshatch)' />",
+                self.margin, self.margin, N, N);
+        let color_light = "fff";
     }
-    else // shaded
-        printf("<rect x='%d' y='%d' width='%d' height='%d' fill='#%s' />\n",
-                margin, margin, N, N, color_dark);
+    else { // shaded
+        println!("<rect x='{}' y='{}' width='{}' height='{}' fill='#{}' />",
+                 self.margin, self.margin, N, N, self.color_dark);
+    }
 
-    printf("<!-- LIGHT SQUARES -->\n");
-    printf("<pattern id='checkerboard' x='%d' y='%d' width='%d' height='%d' patternUnits='userSpaceOnUse'>\n", 
-            margin, margin, 2*units, 2*units);
-    printf("  <rect x='0' y='0' width='%d' height='%d' style='fill:#%s;' />\n", 
-            units, units, color_light);
-    printf("  <rect x='%d' y='%d' width='%d' height='%d' style='fill:#%s;' />\n",
-            units, units, units, units, color_light);
-    printf("</pattern>\n");
-    printf("<rect x='%d' y='%d' width='%d' height='%d' fill='url(#checkerboard)' />\n",
-            margin, margin, N, N);
+    println!("<!-- LIGHT SQUARES -->");
+    println!("<pattern id='checkerboard' x='{}' y='{}' width='{}' height='{}' patternUnits='userSpaceOnUse'>",
+            self.margin, self.margin, 2*self.units, 2*self.units);
+    println!("  <rect x='0' y='0' width='{}' height='{}' style='fill:#{};' />",
+            self.units, self.units, self.color_light);
+    println!("  <rect x='{}' y='{}' width='{}' height='{}' style='fill:#{};' />",
+            self.units, self.units, self.units, self.units, self.color_light);
+    println!("</pattern>");
+    println!("<rect x='{}' y='{}' width='{}' height='{}' fill='url(#checkerboard)' />",
+            self.margin, self.margin, N, N);
 }
 
 
-void 
-put_border ()
-{
-    int n, N;
-    int r = corner;
-    int t = 0; //units / 32; // border gap thickness
-    printf("<!-- BORDER -->\n"); 
-    n = margin - t;
-    N = board + 2*t;
-    printf("<rect x='%d' y='%d' width='%d' height='%d' fill='none' " 
-            "stroke-width='%d' stroke-location='outside' stroke='#fff' rx='%d' ry='%d' />\n",
+fn put_border(&self) {
+    let r = self.corner_radius;
+    let t = 0i32; //units / 32; // border gap thickness
+    println!("<!-- BORDER -->");
+    let mut n = self.margin - t;
+    let board = 8*self.units;
+    let mut N = board + 2*t;
+    println!("<rect x='{}' y='{}' width='{}' height='{}' fill='none' \
+            stroke-width='{}' stroke-location='outside' stroke='#fff' rx='{}' ry='{}' />",
             n, n, N, N, t, r, r);
-    n -= border == 1 ? border : border/2;
-    N += border == 1 ? 2*border : border; 
-    printf("<rect x='%d' y='%d' width='%d' height='%d' fill='none' " 
-            "stroke-width='%d' stroke-location='outside' stroke='#000' rx='%d' ry='%d' />\n",
-            n, n, N, N, border, r, r);
-    //n = margin - border/6; 
-    //N = board + border/3;
-    //printf("<rect x='%d' y='%d' width='%d' height='%d' fill='none' " 
-     //         "stroke-width='%d' stroke='#000' rx='%d' ry='%d' />\n",
-    //        n, n, N, N, border/6, r, r);
+    if self.border == 1 {
+        n -= self.border;
+        N += 2*self.border;
+    } else {
+        n -= self.border / 2;
+        N += self.border;
+    }
+    println!("<rect x='{}' y='{}' width='{}' height='{}' fill='none' \
+            stroke-width='{}' stroke-location='outside' stroke='#000' rx='{}' ry='{}' />",
+            n, n, N, N, self.border, r, r);
 }
 
 
-void
-put_piece (const char c, const int x, const int y)
-{
-    char path[80];
-    printf("<svg x='%d' y='%d' width='%d' height='%d'>\n", x, y, units, units);
-    if (isupper(c))
-        sprintf(path, "svg/%s/w%c.svg", pieces, c);
-    else
-        sprintf(path, "svg/%s/b%c.svg", pieces, toupper(c));
-    cat(path);
-    printf("</svg>\n");
+fn put_piece(&self, c: char, x: i32, y: i32) {
+    println!("<svg x='{}' y='{}' width='{}' height='{}'>\n", x, y, self.units, self.units);
+    if c.is_uppercase() {
+        let path = format!("svg/{}/w{}.svg", self.pieces, c);
+        cat(&path);
+    } else {
+        let path = format!("svg/{}/b{}.svg", self.pieces, c);
+        cat(&path);
+    }
+    println!("</svg>");
 }
 
 
-void 
-put_move_indicator ()
-{
-    int cx = units*8 + 3*margin/2;
-    int cy = margin + units/2;
-    int dy = units/5, dx = units/5;
-    int stroke = 1;
+fn put_move_indicator(&self) {
+    let cx = self.units*8 + 3*self.margin/2;
+    let mut cy = self.margin + self.units/2;
+    let mut dy: i32 = self.units/5;
+    let dx: i32 = self.units/5;
+    let stroke = if self.white_to_move() { 2 } else { 1 };
 
-    if ((whitetomove && !flags.flip) || (blacktomove && flags.flip)) {
-        cy += 7*units;
+    if (self.white_to_move() && !self.flip) ||
+        (self.black_to_move() && self.flip) {
+        cy += 7*self.units;
         dy = -dy;
     }
-    if (whitetomove) {
-        //size -= 1;
-        stroke = 2;
-    }
-    char *fill = whitetomove ? "fff" : "000";
-    printf("<path d='M%d %d l%d %d l%d %d q%d %d %d %d' stroke='black' stroke-width='%d' fill='#%s' />\n",
+    let fill = if self.white_to_move() { "fff" } else { "000" };
+    println!("<path d='M{} {} l{} {} l{} {} q{} {} {} {}' stroke='black' stroke-width='{}' fill='#{}' />",
             cx-dx, cy-dy, dx, 2*dy, dx, -2*dy, -dx, dy, -2*dx, 0, stroke, fill);
 }
 
 
-void
-put_coords ()
-{
-    const char *files = "abcdefgh";
-    int rank_y0 = margin + units/2 + fontsize/2;
-    int rank_x0 = margin - fontsize - border;
-    int file_x0 = margin + units/2 - fontsize/4; 
-    int file_y0 = margin + units*8 + border + fontsize;
-    
-    printf("<svg>\n");
-      printf("<style> .small { font: normal %dpx %s; } </style>\n", fontsize, font);
+    fn put_coords(&self) {
+        let files: Vec<char> = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+        let rank_y0 = self.margin + self.units/2 + self.font_size/2;
+        let rank_x0 = self.margin - self.font_size - self.border;
+        let file_x0 = self.margin + self.units/2 - self.font_size/4;
+        let file_y0 = self.margin + self.units*8 + self.border + self.font_size;
 
-    for (int file = 0; file < 8; ++file)
-        printf("<text x=\"%d\" y=\"%d\" fill=\"#000\" class=\"small\">%c</text>\n", 
-            file_x0+units*file, file_y0, files[flags.flip ? 7-file : file]);
-    for (int rank = 0; rank < 8; ++rank)
-        printf("<text x=\"%d\" y=\"%d\" fill=\"#000\" class=\"small\">%d</text>\n", 
-            rank_x0, rank*units+rank_y0, flags.flip ? rank+1 : 8-rank);
+        println!("<svg>");
+         println!("<style> .small { font: normal {}px {}; } </style>", fontsize, font);
 
-    printf("</svg>\n");
-}
-
-
-void
-build_board (const char *fen)
-{
-    whitetomove = (sidetomove(fen) == 'w');
-    blacktomove = 1 - whitetomove;
-    board = 8*units;
-	int step = flags.flip ? -units : units;
-
-    put_svg_header(fen);
-    put_squares();
-    put_border();
-
-    if (flags.coords) 
-        put_coords();
-
-    int i = 0, x = margin, y = margin;
-	if (flags.flip) {
-		x += 7*units;
-		y += 7*units;
-	}
-	puts("<!-- PIECES -->");
-    for ( ; i < strlen(fen); ++i)
-    {
-        if (isalpha(fen[i])) {
-            put_piece(fen[i], x, y);
-            x += step;
-        } else if (isdigit(fen[i]))
-            x += step*atoi(&fen[i]);
-        else if (fen[i] == '/') {
-            x = flags.flip ? 7*units + margin : margin;
-            y += step;
-        } else if (fen[i] == ' ')
-            break;
-        else
-            err(EX_DATAERR, "Weird FEN received: %s\n", fen);
+        for file in 0..8 {
+            println!("<text x=\"{}\" y=\"{}\" fill=\"#000\" class=\"small\">%c</text>",
+                file_x0+self.units*file, file_y0, files[if self.flip { 7-file } else { file }]);
+        }
+        for rank in 0..8 {
+            println!("<text x=\"{}\" y=\"{}\" fill=\"#000\" class=\"small\">{}</text>",
+                rank_x0, rank*self.units+rank_y0, if self.flip  { rank+1 } else { 8-rank });
+        }
+        println!("</svg>");
     }
-    if (flags.indicator)
-        put_move_indicator();
-    puts("</svg>");
-}
 
 
-impl Board {
-    fn new () -> Board {
-        flags.coords = 0;
-        flags.flip = 0;
-        flags.grayscale = 0;
-        flags.indicator = 0;
-        strlcpy(pieces, "merida", sizeof pieces);
-        units = 64;
-        border = 2;
-        corner = 0; //units/16;
-        margin = 48;
-        fontsize = 20;
-        strlcpy(font, "sans-serif", 64);
-	    // GRAY
-        //strlcpy(color_light, "fff", 4);
-        //strlcpy(color_dark, "bbb", 4);
-	    // BROWN
-        strlcpy(color_light, "a48c62", 7);
-        strlcpy(color_dark, "846c40", 7);
-	    // GREEN: #77955f #eeeed3
-	    // BROWN: #846c40 #a48c62
-        let startfen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    fn build_board(&self) {
+        let board = 8*self.units;
+	    let step: i32 = if self.flip { -self.units } else { self.units };
+
+        self.put_svg_header();
+        self.put_squares();
+        self.put_border();
+
+        if self.show_coords {
+            self.put_coords();
+        }
+
+        let mut x: i32 = self.margin;
+        let mut y: i32 = self.margin;
+	    if self.flip {
+		    x += 7*self.units;
+		    y += 7*self.units;
+	    }
+	    print!("<!-- PIECES -->");
+        for f in self.fen.chars() {
+            if f.is_alphabetic() {
+                self.put_piece(f, x, y);
+                x += step;
+            } else if f.is_numeric() {
+                x += step * f.parse::<i8>().unwrap();
+            } else if f == '/' {
+                x = if self.flip { 7*units + margin } else { margin };
+                y += step;
+            } else if f == ' ' {
+                break;
+            } else {
+                panic!("Weird FEN received: {}", fen);
+            }
+        }
+        if self.indicator {
+            self.put_move_indicator();
+        }
+        puts("</svg>");
     }
+
 }
 
 
 fn print_usage() {
     eprintln!("Usage: svgboard [-c] [-g] [-h] [-p <pieces>] [-r]");
     eprintln!("\t-b\t\t border thickness (default: {})", border);
-    eprintln!("\t-c\t\t turn on coordinates (default: {})", flags.coords);
+    eprintln!("\t-c\t\t turn on coordinates (default: {})", self.coords);
     eprintln!("\t-d\t\t dark square color (default: {})", color_dark);
     eprintln!("\t-f\t\t font size (default: {})", fontsize);
     eprintln!("\t-F\t\t font name (default: {})", font);
@@ -286,13 +266,7 @@ fn print_usage() {
     eprintln!("\t-s \t\t square size (default: {})", units);
 }
 
-int 
-main (int argc, char *argv[])
-{
-    int c;
-
-    // defaults
-    set_defaults();
+fn main() {
 
     let mut b = Board::new();
 
@@ -303,7 +277,7 @@ main (int argc, char *argv[])
             border = atoi(optarg);
             break;
         case 'c':
-            flags.coords = 1;
+            self.coords = 1;
             break;
         case 'd':
             strlcpy(color_dark, optarg, 9);
@@ -315,10 +289,10 @@ main (int argc, char *argv[])
             fontsize = atoi(optarg);
             break;
         case 'g':
-            flags.grayscale = 1;
+            self.grayscale = 1;
             break;
         case 'i':
-            flags.indicator = 1;
+            self.indicator = 1;
             break;
         case 'l':
             strlcpy(color_light, optarg, 9);
@@ -330,7 +304,7 @@ main (int argc, char *argv[])
             strlcpy(pieces, optarg, sizeof pieces);
             break;
         case 'r':
-            flags.flip = 1;
+            self.flip = 1;
             break;
         case 's':
             units = atoi(optarg);
@@ -345,5 +319,4 @@ main (int argc, char *argv[])
     argv += optind;
     build_board(argc > 0 ? *argv : startfen);
 
-    return 0;
 }
